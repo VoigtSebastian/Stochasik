752e458400dbce136b572174d0b9efe1cfc9efb8
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2023-06-06 21:53:42 +0200

    Switch to tabulate to format output

--- 01_beschreibendeStatistik.py
+++ 01_beschreibendeStatistik.py
@@ -3,24 +3,21 @@
 import matplotlib.pyplot as plt
 import math
 import numpy as np
+import tabulate
 
 # -----------------------------------------------------------------------------
 # Setup
 # -----------------------------------------------------------------------------
 
-list = [2, 4, 3, 1, 2, 4, 2, 2, 2, 3]
-quantile = [0.25, 0.3, 0.66]
+list = [25, 10, 30, 25, 35, 25]
+quantile = [0.75]
+output = []
 
 list.sort()
 
 
-def print_separator(sep="_"):
-    [print(sep, end="") for _ in range(64)]
-    print()
-
-
-def print_quantile(v, q):
-    print(f.format(f"{int(v*100)}-Quantil", q))
+def add_quantile(v, q):
+    output.append([f"{int(v*100)}-Quantil", q])
 
 
 def modalwert(list):
@@ -37,32 +34,26 @@ def modalwert(list):
 # Output
 # -----------------------------------------------------------------------------
 
-f = "⎸{:<50}| {:010.4f} ⎸"
-print_separator()
-print(f.format("Minimum", np.min(list)))
-print(f.format("Maximum", np.max(list)))
-print(f.format("Spannweite", np.max(list) - np.min(list)))
-print_separator(sep="-")
-print(f.format("Arithmetisches Mittel / Mittel / Erwartungswert", np.mean(list)))
-print(f.format("Median", np.median(list)))
-print(f.format("Varianz", np.var(list, ddof=1)))
-print(f.format("Standardabweichung", np.std(list, ddof=1)))
-print(f.format("Modalwert", modalwert(list)))
+output.append(["Minimum", np.min(list)])
+output.append(["Maximum", np.max(list)])
+output.append(["Spannweite", np.max(list) - np.min(list)])
+output.append(["Arithmetisches Mittel / Erwartungswert", np.mean(list)])
+output.append(["Median", np.median(list)])
+output.append(["Varianz", np.var(list, ddof=1)])
+output.append(["Standardabweichung", np.std(list, ddof=1)])
+output.append(["Modalwert", modalwert(list)])
 
-print_separator(sep="-")
 
 for quant in quantile:
     quantil = len(list) * quant
     if quantil.is_integer():
         q = np.quantile(list, quant, method="midpoint")
-        print_quantile(quant, q)
+        add_quantile(quant, q)
     else:
         q = list[math.ceil(quant * len(list)) - 1]
-        print_quantile(quant, q)
+        add_quantile(quant, q)
 
-
-print_separator(sep="‾")
-
+print(tabulate.tabulate(output, headers=["Name", "Wert"], tablefmt="fancy_grid"))
 
 # -----------------------------------------------------------------------------
 # Plotting
